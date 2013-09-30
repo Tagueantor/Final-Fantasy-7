eb9fca114ec62131795b55713487095cc1b695ba
Tagueantor <calvin.tague@hotmail.co.uk>
2013-09-30 17:33:08 +0100

    Initial Base Class commit.
    Contains the starting packages and Development text files.
    
    Tag.

---
+++ ff7_common/tagueantor/ff7/FinalFantasy7.java
@@ -0,0 +1,14 @@
+package tagueantor.ff7;
+
+import tagueantor.ff7.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+
+public class FinalFantasy7 {
+
+	
+	
+	
+}
---
+++ ff7_common/tagueantor/ff7/lib/Reference.java
@@ -0,0 +1,11 @@
+package tagueantor.ff7.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "ff7";
+	public static final String MOD_NAME = "Final Fantasy 7";
+	public static final String VERSION = "Alpha v0.1";
+	
+	
+	
+}
