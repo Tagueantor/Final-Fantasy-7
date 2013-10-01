2d6e71f3a07f83575d1575a457a02486e790fa5f
Tagueantor <calvin.tague@hotmail.co.uk>
2013-10-01 11:09:04 +0100

    Developer's notes and some more code.

--- ff7_common/tagueantor/ff7/FinalFantasy7.java
+++ ff7_common/tagueantor/ff7/FinalFantasy7.java
@@ -2,12 +2,35 @@ package tagueantor.ff7;
 
 import tagueantor.ff7.lib.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-
+//Declaring Mod ID, Name and version using Refence file.
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 
 public class FinalFantasy7 {
 
+	//Pre-Initialisation Event
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	};
+	
+	//Initialisation Event
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		
+	};
+	
+	//Post-Initialisation Event
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	};
+	
+	
 	
 	
 	
